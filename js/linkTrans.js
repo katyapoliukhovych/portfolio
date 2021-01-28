78367236d490427cb9105daa94c9703b8906fd88
katyapoliukhovych <o.oxi@ukr.net>
2021-01-28 15:33:58 +0200

    home page

---
+++ js/linkTrans.js
@@ -0,0 +1,16 @@
+var linkTrans = {
+	nav_links: document.querySelectorAll('.nav_link'),
+	social_links: document.querySelectorAll('.social_link'),
+	link_trans_class: 'link_trans',
+
+	start: function() {
+		console.log(this.nav_links);
+		for (var i = 0; i < this.nav_links.length; i++) {
+			this.nav_links[i].classList.add(this.link_trans_class);
+		};
+
+		for (var i = 0; i < this.social_links.length; i++) {
+			this.social_links[i].classList.add(this.link_trans_class);
+		}
+	}
+}
